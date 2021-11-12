7c42d82861279e8d18a689380326859a4ca15730
8080s <88991956+8080s@users.noreply.github.com>
2021-11-12 02:51:43 -0600

    Add files via upload

---
+++ scripts/forecast.js
@@ -0,0 +1,26 @@
+
+const key = 'biWn5f6EZe4voUqSMBGXRcx9xxOF7Uew';
+
+const getCity = async(city) =>{
+
+    const url = 'https://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`
+
+    const response = await fetch(url+query);
+    const data = await response.json();
+    return data[0];
+
+}
+
+
+const getWeather = async(id) =>{
+
+    const url = 'https://dataservice.accuweather.com/currentconditions/v1/'
+    const query = `${id}?apikey=${key}`
+    const response = await fetch(url+query);
+    const data = await response.json();
+
+    return data[0];
+}
+
+
